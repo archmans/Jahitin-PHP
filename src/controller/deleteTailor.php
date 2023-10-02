dc0566e07a6abee10d3482065e65be66d844c222
Muhamad Salman Hakim Alfarisi <13521010@std.stei.itb.ac.id>
2023-10-02 21:58:56 +0700

    add can delete tailor

---
+++ src/controller/deleteTailor.php
@@ -0,0 +1,16 @@
+<?php
+require 'functions.php';
+$tailorID = $_GET["id"];
+
+if( deleteTailor($tailorID) > 0 ) {
+	echo "<script>
+			alert('data berhasil dihapus!');
+			document.location.href = '../homePage.php';
+		</script>";
+} else {
+	echo "<script>
+			alert('data gagal dihapus!');
+			document.location.href = '../homePage.php';
+		</script>";
+}
+?>
