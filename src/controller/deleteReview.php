f144d4a25dd33252f00249f1239557aa96868994
Muhamad Salman Hakim Alfarisi <13521010@std.stei.itb.ac.id>
2023-10-03 17:13:07 +0700

    add page delete review

---
+++ src/controller/deleteReview.php
@@ -0,0 +1,18 @@
+<?php
+require 'functionsReview.php';
+$reviewID = $_GET["id"];
+$penjahit = query("select * from review where reviewID = $reviewID")[0];
+$penjahitID = $penjahit["penjahitID"];
+
+if( deleteReview($reviewID) > 0 ) {
+	echo "<script>
+			alert('data berhasil dihapus!');
+			document.location.href = '../reviewPage.php?id=$penjahitID';
+		</script>";
+} else {
+	echo "<script>
+			alert('data gagal dihapus!');
+			document.location.href = '../reviewPage.php?id=$penjahitID';
+		</script>";
+}
+?>
