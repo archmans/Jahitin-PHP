08dcf9676deef6f0bb2e0d70d8b33e5fa80a9692
lostgirrlll <13521030@std.stei.itb.ac.id>
2023-10-04 17:41:50 +0700

    add halaman review

--- src/reviewPage.php
+++ src/reviewPage.php
@@ -16,41 +16,69 @@ $review = query("select * from review where penjahitID = $idPenjahit");
 <html>
 <head>
 	<title>Halaman Review</title>
+	<meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="styles\homepage.css">
+    <link rel="stylesheet" href="styles\reviewPage.css">
 </head>
-<body>
-<h1>Review Penjahit</h1>
-<a href="homePage.php">Back</a>
-<br><br>
-<a href="controller/addReview.php?id=<?php echo $idPenjahit; ?>">Add Review</a>
-<br><br>
 
-<table border="1" cellspacing="0" cellpadding="5">
-	<tr>
-		<th>No</th>
-		<th>Action</th>
-		<th>Ulasan</th>
-		<th>Foto</th>
-		<th>Video</th>
-	</tr>
-	<?php if ($review == null) { ?>
-	<tr>
-		<td colspan="5" align="center">Review not found</td>
-	</tr>
-	<?php } ?>
-	<?php $i = 1; ?>
-	<?php foreach( $review as $row ) { ?>
-	<tr>
-		<td><?= $i; ?></td>
-		<td>
-			<a href="controller/updateReview.php?id=<?php echo $row["reviewID"]; ?>">Update</a> | <a href="controller/deleteReview.php?id=<?= $row["reviewID"]; ?>" onclick="return confirm('Are you sure?');">Delete</a>
-		</td>
-		<td><?= $row["ulasan"]; ?></td>
-		<td><img src="img/<?= $row["foto_ulasan"]; ?>" width="240"></td>
-		<td><video src="vid/<?= $row["video_ulasan"]; ?>" width="240" controls></td>
-	</tr>
-	<?php $i++; ?>
-	<?php } ?>
-</table>
+<body>
+	<div class="back-container">
+		<a href="homePage.php">Back</a>
+	</div>
+	<div class="bangs">
+		<p class="title"> Hi, <?php echo $_SESSION['username']; ?>! </p>
+	</div>
+	<nav>
+		<div class="logo">
+			<img src="assets\logo.png" alt="logo jahitin"/>
+		</div>
+		<ul>
+			<li><a href="homepageAdmin.php">Tailor</a></li>
+			<li><a href="manageUser.php">User</a></li>
+			<li><a href="logoutBackend.php">Logout</a></li>
+		</ul>
+	</nav>
+	<div class="search-results" style="margin-top: 0rem">
+		<div class="line">
+			<p>Tailor's Review <span id="search-term-text"></span></p>
+		</div>
+	</div>
+	<div class="add">
+		<a href="controller/addReview.php?id=<?php echo $idPenjahit; ?>"> + Add Review</a>
+	</div>
+
+	<div class="styled-container">
+		<?php if ($review == null) { ?>
+			<div class="styled-item" align="center">Review not found</div>
+		<?php } ?>
+		<?php $i = 1; ?>
+		<?php foreach( $review as $row ) { ?>
+			<div class="card-review">
+				<div class="container-review-all">
+					<div class="container-foto">
+						<img src="assets/user.png" style="height: 30px">
+					</div>
+					<div class="container-review">
+						<p class="user"> anonymous </p>
+						<div class="isi-ulasan"><?= $row["ulasan"]; ?></div>
+						<div class="media-ulasan">
+							<img src="img/<?= $row["foto_ulasan"]; ?>" style="height: 100px; max-width=30px">
+							<video src="vid/<?= $row["video_ulasan"]; ?>" style="height: 100px" controls>
+						</div>
+						<div class="aksi">
+							<a class="update" href="controller/updateReview.php?id=<?php echo $row["reviewID"]; ?>">Update</a> 
+							<a class="delete" href="controller/deleteReview.php?id=<?= $row["reviewID"]; ?>" onclick="return confirm('Are you sure?');">Delete</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<?php $i++; ?>
+		<?php } ?>
+	</div>
+
+
 
 </body>
 </html>
