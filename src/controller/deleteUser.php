307a7a8e0ee2515a0c7674067d476eab8d176fb2
Muhamad Salman Hakim Alfarisi <13521010@std.stei.itb.ac.id>
2023-10-06 09:14:23 +0700

    feat : admin and user session delete user

--- src/controller/deleteUser.php
+++ src/controller/deleteUser.php
@@ -1,4 +1,13 @@
 <?php
+session_start();
+if ($_SESSION["role"] != "admin") {
+    header("location: loginPage.php");
+    exit;
+}
+if (!isset($_SESSION["login"])) {
+    header("location: loginPage.php");
+    exit;
+}
 require 'functionsUser.php';
 $userID = $_GET["id"];
 
