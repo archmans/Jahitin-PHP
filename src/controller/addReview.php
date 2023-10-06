3f52043aa2f5678c60b5715f0b2fee7034c59fa2
Muhamad Salman Hakim Alfarisi <13521010@std.stei.itb.ac.id>
2023-10-06 09:13:59 +0700

    feat : admin and user session add review

--- src/controller/addReview.php
+++ src/controller/addReview.php
@@ -50,14 +50,14 @@ if( isset($_POST["tambah"]) ) {
         </div>
         <div class="nav-right">
             <ul>
-				<?php if ($_SESSION['username'] == 'admin') { ?>
+				<?php if ($_SESSION['role'] == 'admin') { ?>
 					<li><a href="homepageAdmin.php">Tailor</a></li>
 					<li><a href="manageUser.php">User</a></li>
-					<li><a href="logoutBackend.php">Logout</a></li>
+					<li><a href="../backend/logout.php">Logout</a></li>
 				<?php } else { ?>
 					<li><a href="homepageUser.php">Home</a></li>
 					<li><a href="profilPage.php">Profile</a></li>
-					<li><a href="logoutBackend.php">Logout</a></li>
+					<li><a href="../backend/logout.php">Logout</a></li>
 				<?php } ?>
             </ul>
         </div>
